c08866e7c16e32274c2030987e99a7b1a1faa8e0
Jean Pool Cruz <jcruzcespedes72@yahoo.com>
2025-02-09 11:52:20 -0600

    New AI Agent was added

---
+++ app/api/chat/route.ts
@@ -0,0 +1,51 @@
+import { openai } from '@ai-sdk/openai';
+import { streamText, tool } from 'ai';
+import { z } from 'zod';
+
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  console.log('OPENAI_API_KEY=', process.env.OPENAI_API_KEY);
+
+  const { messages } = await req.json();
+
+  console.log("Mensajes recibidos:", messages);
+
+  const result = streamText({
+    model: openai('gpt-4o-mini'),
+    messages,
+    tools: {
+      weather: tool({
+        description: 'Get the weather in a location (fahrenheit)',
+        parameters: z.object({
+          location: z.string().describe('The location to get the weather for'),
+        }),
+        execute: async ({ location }) => {
+          const temperature = Math.round(Math.random() * (90 - 32) + 32);
+          return {
+            location,
+            temperature,
+          };
+        },
+      }),
+      convertFahrenheitToCelsius: tool({
+        description: 'Convert a temperature in fahrenheit to celsius',
+        parameters: z.object({
+          temperature: z
+            .number()
+            .describe('The temperature in fahrenheit to convert'),
+        }),
+        execute: async ({ temperature }) => {
+          const celsius = Math.round((temperature - 32) * (5 / 9));
+          return {
+            celsius,
+          };
+        },
+      }),
+    },
+  });
+
+  console.log("Solicitud enviada al modelo GPT");
+
+  return result.toDataStreamResponse();
+}
